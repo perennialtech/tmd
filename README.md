564587adb6d60c3bf092d347cbe52cd36d32fe88
perennial <mail@perennialte.ch>
2026-09-03 12:37:08 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,124 @@
+# Dedicated Server Utils
+This directory contains utilities for a dedicated server on Linux or Docker
+
+---
+
+## ARM64 Linux Support
+
+Linux ARM64 (aarch64) is natively supported as of tModLoader 1.4.4. No extra workarounds are required — you can install directly from Steam or GitHub releases.
+
+If you are using **tModLoader 1.4.3 or earlier**, the Steamworks.NET library did not support ARM in its managed code. For those legacy versions only, use the [20.1.0 Release build on this fork](https://github.com/MikolajKolek/Steamworks.NET-arm64/releases/tag/20.1.0) by [MikolajKolek](https://github.com/MikolajKolek) and replace the existing `.dll` in `Libraries/Steamworks.net/20.1.0/lib/netstandard2.1`.
+---
+
+## Quick Links
+
+### Preinstall Steps (REQUIRED)
+* [Specific Folder Structure](#preinstall-folder-structure)
+
+### Installation
+* [Docker (recommended)](#using-the-docker-container)
+
+* [Management Script](#using-the-management-script)
+
+* [Installing Mods](#mod-installation)
+
+
+[Server Configuration](#server-configuration)
+
+[Updating the Management Script](#updating-the-management-script)
+
+## Preinstall Folder Structure
+Both the Docker and Management script **require** the same folder structure, so make sure it's setup properly before installing. Here is what is should look like:
+
+```
+docker-compose.yml (Docker version only)
+Dockerfile (Docker version only)
+tModLoader
+├── Mods
+│   ├── localmod1.tmod
+│   ├── localmod2.tmod
+│   ├── enabled.json
+│   └── install.txt
+├── Worlds
+│   ├── world1.twld
+│   ├── world1.wld
+│   ├── world2.twld
+│   └── world2.wld
+├── server (automatically created, management script version only)
+│   └── * contains tModLoader installation *
+├── steamapps (automatically created)
+│   └── * contains Steam workshop mods *
+├── manage-tModLoaderServer.sh (management script version only)
+├── serverconfig.txt (optional)
+└── tmlversion.txt (optional)
+```
+
+### Obtaining install.txt and enabled.json
+`install.txt` and `enabled.json` are needed for any mods you wish to install from the Steam Workshop.
+The steam workshop does not use mod names to identify mods, so a modpack should be made to install mods from the workshop
+1. From the TML main menu, go to Workshop -> Mod Packs
+2. Click `Save Enabled as New Mod Pack`
+3. Click `Open Mod Pack Folder`
+4. Enter the folder with the name of your modpack
+5. Make a `Mods` folder and copy `install.txt` and `enabled.json` file into it
+6. **Management Script Version Only**: Run `./manage-tModLoaderServer.sh install-mods` to install the mods on your server
+
+---
+
+## Using The Docker Container
+### Installation
+1. First, ensure you have a proper [folder structure](#preinstall-folder-structure)
+   * It is also **highly** recommended to create a [serverconfig.txt](#server-configuration) file. This makes it so when you start the container you don't have to attach to manually configure the server
+2. Install `docker` from your package manager or [Docker's Official Page](https://docs.docker.com/engine/install/)
+   * **To check if Compose V2 is installed in this package**, run `docker compose version`. If the command errors, your manager still uses V1 and will need to additionally install the `docker-compose` package. All commands below assume Compose V2 is installed, so if you have V1 replace any `docker compose` commands with `docker-compose`
+3. Download [docker-compose.yml](https://github.com/tModLoader/tModLoader/tree/1.4.4/patches/tModLoader/Terraria/release_extras/DedicatedServerUtils/docker-compose.yml) and the [Dockerfile](https://github.com/tModLoader/tModLoader/tree/1.4.4/patches/tModLoader/Terraria/release_extras/DedicatedServerUtils/Dockerfile)
+4. Edit `docker-compose.yml` with your GID and UID. These can be found by running `id`, and generally default to 1000
+   * **IMPORTANT:** Make sure all files in the `tModLoader` folder have this same UID/GID. This can be set by running `chown -R UID:GID tModLoader`
+   * You can optionally also set the `TMLVERSION` arg to get a specific tModLoader version
+5. Run `docker compose up -d`. This command will create the `Mods` and `Worlds` directories if they don't already exist
+   * To attach to the server console run `docker attach tml`. To detach from the console press `Ctrl-P Ctrl-Q` to avoid shutting down or `Ctrl-C` to detach and shutdown the server
+
+### Running Commands
+To run commands inside the container, run `docker exec -it tml execute "YOUR COMMAND"`. An example hello world would be `docker exec -it tml execute "say Hello World!"`. The quotes are required around the entire command because tmux can only accept one argument to be passed, otherwise the command is sent as a single word without spaces
+
+### Updating
+To update, download the newest container and rebuild it using `docker compose build` to update tModLoader. Mods will be updated as well. If the container doesn't rebuild you can force rebuilding by running `docker compose build --no-cache`
+
+---
+
+## Using The Management Script
+The `manage-tModLoaderServer.sh` script can be used to install tModLoader either directly from the GitHub release or from SteamCMD. The script is made to run fully standalone, so just download it to your server and run it. This is not the recommended way to install the dedicated server, and typically is only used to access the server's files
+
+To explore all the options before continuing, run `./manage-tModLoaderServer.sh -h` to get a list of all environment variables and command line parameters
+
+### Installation
+1. Ensure you have a proper [folder structure](#preinstall-folder-structure)
+2. Install either the SteamCMD or Github release
+   * **SteamCMD (recommended)** 
+      1. Ensure SteamCMD is installed and on your PATH. You can install SteamCMD from your package manager or [Valve's Wiki](https://developer.valvesoftware.com/wiki/SteamCMD). If your distribution cannot install SteamCMD the standard way, download it manually and pass the `STEAMCMDPATH` environment variable to the management script
+      2. Run `./manage-tModLoaderServer.sh install-tml --username your_steam_username` and enter any password/2fa if necessary. tModLoader will install to the `server` directory in your installation folder
+   
+   * **Github**
+      1. Run `./manage-tModLoaderServer.sh install-tml --github`. This will install the latest GitHub release, which is the same version as released on Steam
+         * To use a specific/legacy tModLoader version from Github, provide either a `tmlversion.txt` file from a modpack or pass the `TMLVERSION` environment variable with a specific version, e.g. `v2022.06.96.4`
+3. Install any necessary mods with `./manage-tModLoaderServer.sh install-mods`
+   * No mods will be installed if `install.txt` is missing, and no mods will be enabled if `enabled.json` is missing. **You will need a `Mods/enabled.json` to contain all Mods that you want enabled, including local mods**
+4. Start the server with `./manage-tModLoaderServer.sh start`. Be sure to pass in `--folder` again if you used a custom location during installation
+
+**NOTE**: `install-mods` and `install-tml` can be run together with the `install` command, e.g. `./manage-tModLoaderServer.sh install ...`
+
+### Updating
+`./manage-tModLoaderServer.sh install` will update both TML and installed mods. To update just mods, run the `install-mods` command. To only update TML, run the `install-tml` command
+
+---
+
+## Server Configuration
+To run tModLoader without needing any input on startup (such as from an init system), copy the example [serverconfig.txt](https://github.com/tModLoader/tModLoader/tree/1.4.4/patches/tModLoader/Terraria/release_extras/serverconfig.txt) and change the settings how you like. Key options are defined below, and other options can be found [on the Terraria wiki](https://terraria.wiki.gg/wiki/Server#Server_config_file)
+* `worldname` changes the default world name when creating a new world using autocreate **You do not need to include .wld in your world name**. This setting **will not** work with an existing world, see the `world` option for an existing world
+* `world` sets the exact path to an existing or new terraria world, ex. `/path/to/your_world.wld`. **For Docker installations**, the world path must follow `/tModLoader/Worlds/your_world.wld`
+* `autocreate=1` will enable autocreating, which creates a new world at your provided location if one does not already exist
+
+---
+
+## Updating the Management Script
+If an update for `manage-tModLoaderServer.sh` is available, a message will be printed letting you know one is available. It can be updated using `./manage-tModLoaderServer.sh update-script`. An outdated script may contain bugs or lack features, so it is usually a good idea to update when possible
---
+++ manage-tModLoaderServer.sh
@@ -0,0 +1,392 @@
+#!/usr/bin/env bash
+
+#shellcheck disable=2164
+#shellcheck disable=2155
+
+# Only update the major version when a breaking change is introduced
+script_version="4.0.0.1"
+script_url="https://raw.githubusercontent.com/tModLoader/tModLoader/1.4.5/patches/tModLoader/Terraria/release_extras/DedicatedServerUtils/manage-tModLoaderServer.sh"
+
+# Shut up both commands
+function pushd {
+	command pushd "$@" > /dev/null || return
+}
+
+function popd {
+	command popd > /dev/null || return
+}
+
+# Copied from BashUtils.sh so there's no need for a dependency on it
+function machine_has {
+	command -v "$1" > /dev/null 2>&1
+	return $?
+}
+
+# There is seemingly no official documentation on this file but other "official" software does this same check.
+# See: https://github.com/moby/moby/blob/v27.4.0/libnetwork/drivers/bridge/setup_bridgenetfiltering.go#L92-L95
+function is_in_docker {
+	if [[ -v ISDOCKER ]] || [[ -f /.dockerenv ]] || [[ -f /run/.containerenv ]]; then
+		return 0
+	fi
+	return 1
+}
+
+function update_script {
+	latest_script_version=$(curl -s "$script_url" 2>/dev/null | grep "script_version=" | head -n1 | cut -d '"' -f2)
+
+	local new_version=$(echo -e "$script_version\n$latest_script_version" | sort -rV | head -n1)
+	if [[ "$script_version" = "$new_version" ]]; then
+		echo "No script update found"
+		return
+	fi
+
+	if is_in_docker; then
+		echo "The management script has been updated, please rebuild your Docker container to get the updated script!"
+		return
+	fi
+
+	if [[ "${script_version:0:1}" != "${new_version:0:1}" ]]; then
+		read -t 20 -p "A major version change has been detected (v$script_version -> v$new_version) Major versions mean incompatibilities with previous versions, so you should check the wiki for any updates to how the script works. Update anyways? (y/n): " update_major
+		if [[ "$update_major" != [Yy]* ]]; then
+			echo "Skipping major version update"
+			return
+		fi
+	else
+		read -t 10 -p "An update for the management script is available (v$script_version -> v$new_version). Update now? (y/n): " update_minor
+		if [[ "$update_minor" != [Yy]* ]]; then
+			echo "Skipping version update"
+			return
+		fi
+	fi
+
+	# Go to where the script currently is
+	pushd "$(dirname "$(realpath "$0")")"
+
+	echo "Updating from version v$script_version to v$latest_script_version"
+	curl -s -O "$script_url" || exit 1
+	mv manage-tModLoaderServer.sh.1 manage-tModLoaderServer.sh
+
+	popd
+}
+
+# Check PATH and flags for required commands for tml/mod installation
+function verify_steamcmd {
+	# Prioritize an ENV Variable
+	if [[ -v STEAMCMDPATH ]]; then
+		if ! [[ -f "$STEAMCMDPATH" ]]; then
+			echo "STEAMCMDPATH is set to a file that does not exist"
+			exit 1
+		fi
+		steam_cmd="$STEAMCMDPATH"
+		return
+	fi
+
+	if [[ -v steamcmd_path ]]; then
+		if ! [[ -f "$steamcmd_path" ]]; then
+			echo "--steamcmdpath is set to a file that does not exist"
+			exit 1
+		fi
+		steam_cmd="$steamcmd_path"
+		return
+	fi
+
+	steam_cmd=$(command -v steamcmd)
+	if [[ -z "$steam_cmd" ]]; then
+		echo "steamcmd could not be found in PATH, please install steamcmd or provide the STEAMCMDPATH environment variable"
+		exit 1
+	fi
+}
+
+function get_version {
+	if [[ -v TMLVERSION ]]; then
+		echo "$TMLVERSION"
+	elif [[ -v tml_version ]]; then
+		echo "$tml_version"
+	elif [[ -r "$folder/Mods/tmlversion.txt" ]]; then
+		# Format the tmlversion file appropriately, as it is missing padded 0's on months/days
+		echo "v$(cat "$folder/Mods/tmlversion.txt" | sed -E 's/\.([0-9])\./\.0\1\./g')"
+	else
+		# Get the latest release if no other options are provided
+		local release_url="https://api.github.com/repos/tModLoader/tModLoader/releases/latest"
+		local latest_release
+		latest_release=$(curl -s "$release_url" 2>/dev/null | grep '"tag_name":' | sort | tail -1 | sed -E 's/.*"([^"]+)".*/\1/') # Get latest release from github's api
+		echo "$latest_release"
+	fi
+}
+
+function install_tml_github {
+	echo "Installing TML from Github"
+	local ver="$(get_version)"
+
+	# Allow nullglob so taht if "v*.tar.gz" matches no entries it doesnt try to remove the literal name
+	shopt -s nullglob
+
+	# If .ver exists we're doing an update instead, compare versions to see if it's already installed and backup if it isn't
+	if [[ -r .ver ]]; then
+		local oldver="$(cat .ver)"
+		if [[ "$ver" = "$oldver" ]]; then
+			echo "Current tModLoader version ($ver) is up to date!"
+			return
+		fi
+
+		echo "New version $ver is wanted, current version is $oldver"
+
+		# Backup old tML versions in case something implodes
+		mkdir "$oldver"
+		for file in *; do
+			if ! [[ "$file" = "manage-tModLoaderServer.sh" ]] && ! [[ "$file" = v*.tar.gz ]] && ! [[ "$file" = "$oldver" ]]; then
+				mv "$file" "$oldver" || exit 1
+			fi
+		done
+
+		# Delete all backups but the most recent if we aren't keeping them
+		if ! $keep_backups; then
+			echo "Removing old backups"
+			for file in v*.tar.gz; do
+				rm "$file" || exit 1
+				echo "Removed old version $file"
+			done
+		fi
+
+		echo "Compressing $oldver backup"
+		tar czf "$oldver.tar.gz" "$oldver"/*
+		rm -r "$oldver"
+	fi
+
+	shopt -u nullglob
+
+	echo "Downloading version $ver"
+	curl -s -LJO "https://github.com/tModLoader/tModLoader/releases/download/$ver/tModLoader.zip" || exit 1
+	echo "Unzipping tModLoader.zip"
+	unzip -q tModLoader.zip
+	rm tModLoader.zip
+	echo "$ver" > .ver
+}
+
+function install_tml_steam {
+	echo "Installing TML from Steam"
+
+	if ! [[ -v username ]]; then
+		echo "Provide the --username flag in order to download TML from Steam"
+		exit 1
+	fi
+
+	# Installs tML, but all other steam assets will be in $HOME/Steam or $HOME/.steam
+	eval "$steam_cmd +force_install_dir $folder/server +login $username +app_update 1281930 +quit"
+
+	if [[ $? = "5" ]]; then
+		echo "Try entering password/2fa code again"
+		install_tml_steam
+	fi
+}
+
+function install_tml {
+	mkdir -p server
+	pushd server
+
+	if $github; then
+		install_tml_github
+	else
+		verify_steamcmd
+		install_tml_steam
+	fi
+
+	if [[ -f "$folder/serverconfig.txt" ]]; then
+		if [[ -f "serverconfig.txt" ]]; then
+			echo "Removing duplicate serverconfig.txt"
+			rm serverconfig.txt
+		fi
+	fi
+
+	popd
+
+	if ! is_in_docker; then
+		echo "Creating folder structure"
+		mkdir -p Mods Worlds
+	fi
+
+	# Install .NET
+	root_dir="$folder/server"
+	LogFile="$folder/server/tModLoader-Logs/DotNet.log"
+	if [[ -f "$root_dir/LaunchUtils/DotNetVersion.sh" ]]; then
+		. "$root_dir/LaunchUtils/DotNetVersion.sh"
+		chmod a+x "$root_dir/LaunchUtils/InstallDotNet.sh" && bash "$_"
+	else
+		echo "WARNING: .NET could not be pre-installed due to missing scripts. It should install on server start."
+	fi
+}
+
+function install_workshop_mods {
+	verify_steamcmd
+	
+	if ! [[ -d "Mods" ]]; then
+		echo "Mods folder does not exist, please run install-tml command or create a 'Mods' folder before installing mods"
+		exit
+	fi
+
+	pushd Mods
+
+	if ! [[ -r install.txt ]]; then
+		echo "No workshop mods to install"
+		popd
+		return
+	fi
+
+	echo "Installing workshop mods"
+
+	local steamcmd_command
+	local lines=0
+	while read -r line; do
+		lines=$((lines + 1))
+		steamcmd_command="$steamcmd_command +workshop_download_item 1281930 $line"
+	done <install.txt 
+
+	eval "'$steam_cmd' +force_install_dir '$folder' +login anonymous$steamcmd_command +quit"
+
+	echo -e "\nInstalled $lines mods"
+
+	popd
+}
+
+function print_help {
+	echo \
+"tML dedicated server installation and maintenance script
+
+Usage: script.sh COMMAND [OPTIONS]
+
+ENV Variables:
+ STEAMCMDPATH        Custom path for the steamcmd binary if your package manager does not have it
+ TMLVERSION          TML version to download. By default this is the latest release
+
+Options:
+ -h|--help           Show command line help
+ -v|--version        Display the current version of the management script
+ -g|--github         Download tML from Github instead of using steamcmd
+ -f|--folder         The folder containing all of your server data (Mods, Worlds, serverconfig.txt, etc..)
+ -u|--username       The steam username to login use when downloading tML. Not required to download mods
+ --keepbackups       When installing with --github, keep all previous versions instead of deleting them when updating
+ --tmlversion        Version of tModLoader to install. Only works if --github is provided. Functionally equivalent to the TMLVERSION env variable
+ --steamcmdpath      Path to steamcmd.sh for Steam tModLoader mod installation. Functionally equivalent to the STEAMCMDPATH env variable
+ --tml-version       DEPRECATED: Kept for compatibility, but the --tmlversion flag or the TMLVERSION environment variable should be used instead
+
+Commands:
+ install-tml         Installs tModLoader from Steam (or Github if --github is provided)
+ install-mods        Installs any mods from install.txt, if present. Requires steamcmd
+ install             Alias for install-tml install-mods
+ start [args]        Launches the server and passes through any extra args
+"
+	exit
+}
+
+github=false
+keep_backups=false
+start_args=""
+
+if [ $# -eq 0 ]; then # Check for commands
+	echo "No command supplied"
+	print_help
+fi
+
+# Covers cases where you only want to provide -h or -v without a command
+cmd="$1"
+if [[ "${cmd:0:1}" != "-" ]]; then
+	shift
+fi
+
+while [[ $# -gt 0 ]]; do
+	case $1 in
+		-h|--help)
+			print_help
+			;;
+		-v|--version)
+			echo "tML Dedicated Server Tool v$script_version"
+			exit
+			;;
+		-g|--github)
+			github=true
+			;;
+		-f|--folder)
+			folder="$2"
+			shift
+			;;
+		-u|--username)
+			username="$2"
+			shift
+			;;
+		--keepbackups)
+			keep_backups=true
+			;;
+		--tmlversion|--tml-version)
+			tml_version="$2"
+			github=true
+			shift
+			;;
+		--steamcmdpath)
+			steamcmd_path="$2"
+			shift
+			;;
+		*)
+			start_args="$start_args $1"
+			;;
+	esac
+	shift
+done
+
+if ! machine_has "curl"; then
+	echo "curl must be installed for the management script to work"
+	exit 1
+fi
+
+update_script
+
+if ! [[ -v folder ]]; then
+	echo "Setting folder to current directory"
+	folder="$(dirname "$(realpath "$0")")"
+fi
+
+mkdir -p "$folder" && pushd "$_"
+
+case $cmd in
+	update-script)
+		# NOOP because the script automatically checks for an update before thiss
+		;;
+	install-mods)
+		install_workshop_mods
+		;;
+	install-tml)
+		install_tml
+		;;
+	install)
+		install_tml
+		install_workshop_mods
+		;;
+	start)
+		# Edge-case for ScriptCaller.sh where dotnet exists but TML logs don't yet
+		export SKIP_DOTNET_LOGCHECK=1
+
+		if is_in_docker; then
+			mkdir -p "$folder/Mods" "$folder/Worlds"
+			install_workshop_mods
+
+			cat "dotnet installed via management script... pending first server start..." >> "$HOME/server/tModLoader-Logs/server.log"
+			cd "$HOME/server" || exit
+		elif ! [[ -f "$folder/server/LaunchUtils/ScriptCaller.sh" ]]; then
+			echo "A tModLoader server is not installed yet, please run the install or install-tml command before starting a server"
+			exit 1
+		else
+			cd "$folder/server" || exit
+		fi
+
+		# NOTE: Technically BASH_SOURCE is >= Bash 3.0, but this version is over 20 years old so the chance of any issues is minimal
+		sed -i 's|cd "$(dirname "$0")"|cd "$(dirname "${BASH_SOURCE[0]}")"|' ./LaunchUtils/ScriptCaller.sh
+
+		chmod +x ./LaunchUtils/ScriptCaller.sh
+		source ./LaunchUtils/ScriptCaller.sh -server -config "$folder/serverconfig.txt" -steamworkshopfolder "$folder/steamapps/workshop" -tmlsavedirectory "$folder" $start_args
+		;;
+	*)
+		echo "Invalid Command: $1"
+		print_help
+		;;
+esac
+
+popd
